9eca3037e2670efd974206222227c769b34cba65
hamda hassan <hamda6464@gmail.com>
2025-05-31 21:53:43 +0100

    changed vnet for dept_Vent

--- dept_Vent.tf
+++ dept_Vent.tf
@@ -1,10 +1,14 @@
 
+/*
+commenting this out, I want all subnets to be under same vnet for now (31/5/25)
+
 resource "azurerm_virtual_network" "dept_vnet" {
   name = var.dept_vnet_name
   location = var.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space = var.dept_vnet
 }
+*/
 
 resource "azurerm_subnet" "department_subnets" {
    
@@ -12,7 +16,7 @@ resource "azurerm_subnet" "department_subnets" {
 
    name = "${each.key}-subnet"
    resource_group_name = azurerm_resource_group.rg.name
-   virtual_network_name = var.dept_vnet_name
+   virtual_network_name = var.vnet_name
    address_prefixes = [each.value]
 
 }
